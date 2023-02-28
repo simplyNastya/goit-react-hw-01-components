65fb8cf64e6796eb3e4f40ee0d6ac27a3840ec8d
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-02-28 17:48:34 +0100

    Refactored code
    Added style to second task

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,15 +3,15 @@ import styles from './statistics.module.css';
 
 const Statistics = ({ title, stats = [] }) => {
   const elements = stats.map(({ id, label, percentage }) => (
-    <li key={id} className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}%</span>
+    <li key={id} className={styles.item}>
+      <span className={styles.label}>{label}</span>
+      <span className={styles.percentage}>{percentage}%</span>
     </li>
   ));
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">{elements}</ul>
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+      <ul className={styles.statlist}>{elements}</ul>
     </section>
   );
 };
